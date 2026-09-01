ecb93f9386392c11d0b76cc70e8cffe83dbb57cc
unknown <maroinefridi>
2026-09-02 01:10:58 +0200

    correction

--- variables/challeng6.js
+++ variables/challeng6.js
@@ -1,21 +1,24 @@
-var prompt = require('prompt-sync')();
-let score1 = Number(prompt('entrer Score partie 1 :'));
 
-var prompt = require('prompt-sync')();
-let score2 = Number(prompt('entrer Score partie 2 :'));
 
-var prompt = require('prompt-sync')();
-let score3 = Number(prompt('entrer Score partie 3 :'));
+let prompt = require('prompt-sync')();
+ score1 = +prompt('entrer Score partie 1 :');
 
-var prompt = require('prompt-sync')();
-let score4 = Number(prompt('entrer Score partie 4 :'));
+prompt = require('prompt-sync')();
+ score2 = +prompt('entrer Score partie 2 :');
+
+prompt = require('prompt-sync')();
+ score3 = +prompt('entrer Score partie 3 :');
+
+prompt = require('prompt-sync')();
+ score4 = +prompt('entrer Score partie 4 :');
 
 console.log("Partie 1 : " + score1);
 console.log("Partie 2 : " + score2);
 console.log("Partie 3 : " + score3);
 console.log("Partie 4 : " + score4);
 
-let Scoretotal = score1 + score2 + score3 + score4;
+let Scoretotal = 0 ;
+Scoretotal += score1 + score2 + score3 + score4;
 
 console.log("score total : " + Scoretotal );
 
--- variables/challeng7.js
+++ variables/challeng7.js
@@ -1,11 +1,11 @@
-var prompt = require('prompt-sync')();
-let noteCC = Number(prompt('entrer note de Contrôle continu : '));
+let prompt = require('prompt-sync')();
+let noteCC = prompt('entrer note de Contrôle continu : ');
 
-var prompt = require('prompt-sync')();
-let noteProjet = Number(prompt('entrer note de projet : '));
+let projet = require('prompt-sync')();
+let noteProjet = projet('entrer note de projet : ');
 
-var prompt = require('prompt-sync')();
-let noteExamen = Number(prompt('entrer note de Examen final : '));
+let exam = require('prompt-sync')();
+let noteExamen = exam('entrer note de Examen final : ');
 
 console.log("Contrôle continu : " + noteCC);
 console.log("Projet : " + noteProjet);
@@ -13,5 +13,3 @@ console.log("Examen final : " + noteExamen);
 
 let Notefinale = (noteCC * 2 + noteProjet * 3 + noteExamen * 5) / 10;
 console.log("Note finale : " + Notefinale);
-
-
