c80abea0f8a7819e7a64734456ea9d067127be20
maroinefridi <maroinehunter@gmail.com>
2026-09-03 18:01:09 +0200

    add

---
+++ day3/fonctions/challenge_1.js
@@ -0,0 +1,7 @@
+//Le Salueur Automatique
+ function afficherBienvenue(){
+    return "Bienvenue dans le cours de JavaScript ! Bonne chance.";
+}
+
+let afficher = afficherBienvenue() ;
+console.log(afficher);
