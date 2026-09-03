e6eaed92bd2212b64fc1326f932e95f7ca7420dc
unknown <maroinefridi>
2026-09-04 00:55:29 +0200

    challenge 2 fix

--- day3/fonctions/challenge_2.js
+++ day3/fonctions/challenge_2.js
@@ -1,1 +1,9 @@
 //Calculateur d'Âge Canin
+let prompt = require(`prompt-sync`)();
+let ageHumain = prompt(`entrer age humain : `);
+let Agechien = CalculerAgeChien();
+
+function CalculerAgeChien(ageHuman){
+    return ageHumain * 7 ;
+}
+console.log(`L'âge du chien équivalent est : ${Agehien} ans.`);
